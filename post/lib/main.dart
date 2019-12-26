643c54fc0f27271d3cc379a64d6dc8f273d43d27
Jaimin020 <48099245+Jaimin020@users.noreply.github.com>
2019-12-26 21:54:49 +0530

    Flutter Project

---
+++ post/lib/main.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import './ui/Home.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      title:"Post Page",
+      home: new Home(),
+    )
+  );
+}
